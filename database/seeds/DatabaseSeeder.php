54a0de4d8865d6356d745c9d346f7ca1354a6d42
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-15 18:35:25 +0200

    Created empty seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(NewsTableSeeder::class);
+		$this->call(CommentsTableSeeder::class);
+		$this->call(ArtistsTableSeeder::class);
+		$this->call(UsersTableSeeder::class);
+        $this->call(TicketsTableSeeder::class);
     }
 }
