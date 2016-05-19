77e65259f86817f1c85706a714362b1c7801ce11
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-19 09:20:00 +0200

    HomeController@inex: returns newly made index.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\News;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,6 +16,6 @@ class HomeController extends Controller
 	 */
     public function index()
 	{
-
+		return view('index');
 	}
 }
