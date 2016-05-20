20845e25d5b408bd42a6e6edb32611434f93d9ed
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-20 09:15:02 +0200

    Created ImageController & Added show action

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class ImageController extends Controller
+{
+	/**
+	 * Display the specified image.
+	 *
+	 * @param string $type
+	 * @param int $filename
+	 * @return Response
+	 */
+    public function show($type, $filename)
+	{
+		if (!in_array($type, ['artist', 'user']))
+		{
+			abort(404);
+		}
+
+		$path = storage_path() . "/app/{$type}_images/{$filename}";
+
+		$paths = glob($path . '.*');
+		if (count($paths) == 1)
+		{
+			$path = $paths[0];
+		}
+
+		if(!File::exists($path))
+		{
+			$path = storage_path() . "/app/{$type}_images/none.png";
+		}
+
+		$file = File::get($path);
+		$contentType = File::mimeType($path);
+
+		$response = Response::make($file, 200);
+		$response->header("Content-Type", $contentType);
+
+		return $response;
+	}
+}
