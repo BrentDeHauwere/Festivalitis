985c1965f1ffd2e3f5785c3e9bf36277b6970adf
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-23 19:48:13 +0200

    User extends Authenticatable (Illuminate\Foundation\Auth\User) & UserMiddleware activated

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,12 +16,14 @@ Route::get('login','AuthController@index');
 Route::post('login','AuthController@login');
 Route::get('logout','AuthController@logout');
 
-// ---------- WEBSITE ----------
-Route::get('/', 'HomeController@index');
-Route::resource('artist', 'ArtistController', ['only' => ['store', 'update', 'destroy']]);
-Route::resource('comment', 'CommentController', ['only' => ['store', 'update', 'destroy']]);
-Route::resource('news', 'NewsController', ['only' => ['store', 'update', 'destroy']]);
-Route::resource('ticket', 'TicketController', ['only' => ['store']]);
-Route::resource('user', 'UserController', ['only' => ['store']]);
-Route::get('image/{type}/{filename}', 'ImageController@show');
-Route::post('mail/send', 'MailController@send');
+Route::group(['middleware' => 'user'], function () {
+	// ---------- WEBSITE ----------
+	Route::get('/', 'HomeController@index');
+	Route::resource('artist', 'ArtistController', ['only' => ['store', 'update', 'destroy']]);
+	Route::resource('comment', 'CommentController', ['only' => ['store', 'update', 'destroy']]);
+	Route::resource('news', 'NewsController', ['only' => ['store', 'update', 'destroy']]);
+	Route::resource('ticket', 'TicketController', ['only' => ['store']]);
+	Route::resource('user', 'UserController', ['only' => ['store']]);
+	Route::get('image/{type}/{filename}', 'ImageController@show');
+	Route::post('mail/send', 'MailController@send');
+});
--- app/User.php
+++ app/User.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Model
+class User extends Authenticatable
 {
 	/**
 	 * The attributes that aren't mass assignable.
