2501dd7fc523f2ec5c8d56d7d0486a7ed6a07219
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2016-05-26 16:11:10 +0200

    Fix: read cookie when not available

---
+++ resources/views/configuration_panel.blade.php
@@ -0,0 +1,124 @@
+@extends('master')
+
+@section('script')
+	<script>
+		$(document).ready(function ()
+		{
+			$('.menu .item')
+				.tab()
+			;
+
+			$('#ConfigurationPanel .tabular.menu .item').click(function ()
+			{
+				document.cookie = 'activeTab=' + $(this).attr('data-tab');
+			});
+		});
+	</script>
+@endsection
+
+@section('content')
+	<div class="ui segment" id="ConfigurationPanel">
+		<h1 class="ui header teal">
+			<i class="configure icon"></i>
+			<div class="content">
+				Configuration Panel
+			</div>
+		</h1>
+
+		<div class="ui top attached tabular menu">
+			<a class="item {{ (empty($_COOKIE['activeTab']) || $_COOKIE['activeTab'] == 'accounts') ? 'active' : '' }}" data-tab="accounts">Accounts</a>
+			<a class="item {{ (!empty($_COOKIE['activeTab']) && $_COOKIE['activeTab'] == 'artists') ? 'active' : '' }}" data-tab="artists">Artists</a>
+			<a class="item {{ (!empty($_COOKIE['activeTab']) && $_COOKIE['activeTab'] == 'news') ? 'active' : '' }}" data-tab="news">News</a>
+		</div>
+
+		<!-- Add Account -->
+		<div class="ui bottom attached tab segment active" data-tab="accounts">
+			<form class="ui form group" action="{{action('UserController@store')}}" method="post">
+
+				<div class="field required">
+					<label for="fname">First Name</label>
+					<input id="fname" type="text" name="fname" value="{{ old('fname') }}" placeholder="First Name" required>
+				</div>
+
+				<div class="field required">
+					<label for="lname">Last Name</label>
+					<input id="lname" type="text" name="lname" value="{{ old('lname') }}" placeholder="Last Name" required>
+				</div>
+
+				<div class="field required">
+					<label for="email">Email</label>
+					<input id="email" type="email" name="email" value="{{ old('email') }}" placeholder="Email" required>
+				</div>
+
+				<div class="field required">
+					<label for="password">Password</label>
+					<input id="password" type="password" name="password" placeholder="Password" required>
+				</div>
+
+				<div class="field required">
+					<label for="password_confirmation">Confirm password</label>
+					<input id="password_confirmation" type="password" name="password_confirmation" placeholder="Confirm password" required>
+				</div>
+
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<button class="ui button primary right floated" type="submit">Create User</button>
+			</form>
+		</div>
+
+		<!-- Add Artist -->
+		<div class="ui bottom attached tab segment" data-tab="artists">
+			<form class="ui form group" action="{{action('ArtistController@store')}}" method="post">
+
+				<div class="field required">
+					<label for="name">Name</label>
+					<input id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Name" required>
+				</div>
+
+				<div class="field required">
+					<label for="description">Description</label>
+					<textarea id="description" name="description" placeholder="Description" required>{{ old('description') }}</textarea>
+				</div>
+
+				<div class="field required">
+					<label for="begin">Begin</label>
+					<input id="begin" type="time" name="begin" value="{{ old('begin') }}" placeholder="Begin" required>
+				</div>
+
+				<div class="field required">
+					<label for="end">End</label>
+					<input id="end" type="time" name="end" value="{{ old('End') }}" placeholder="End" required>
+				</div>
+
+				<div class="field required">
+					<label for="image">Image</label>
+					<input id="image" type="file" name="image" value="{{ old('image') }}" required>
+				</div>
+
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<button class="ui button primary right floated" type="submit">Create Artist</button>
+			</form>
+		</div>
+
+		<!-- Add News -->
+		<div class="ui bottom attached tab segment" data-tab="news">
+			<form class="ui form group" action="{{action('NewsController@store')}}" method="post">
+
+				<div class="field required">
+					<label for="title">Title</label>
+					<input id="title" type="text" name="title" value="{{ old('title') }}" placeholder="Title" required>
+				</div>
+
+				<div class="field required">
+					<label for="description">Description</label>
+					<textarea id="description" name="description" placeholder="Description" required>{{ old('description') }}</textarea>
+				</div>
+
+				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+				<button class="ui button primary right floated" type="submit">Create Artist</button>
+			</form>
+		</div>
+	</div>
+@endsection
